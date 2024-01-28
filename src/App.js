f14146af481734d6f943765ce02553e31b0762f9
username-copied <shreyas.waykar123@gmail.com>
2024-01-28 13:13:32 +0530

    1s5 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import DocxViewer from './components/DocxViewer';
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const docs = [
+    // { uri: "https://url-to-my-pdf.pdf" }, // Remote file
+    { uri: require("./files/docsx.docx") }, // Local File
+  ];
+
+  return <>
+    <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
+    <DocxViewer/>
+    </>;
+
 }
 
 export default App;
---
+++ src/components/DocxViewer.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import DocViewer,{DocViewerRenderers,MSDocRenderer,CSVRenderer} from '@cyntler/react-doc-viewer';
+
+const DocxViewer = () => {
+  const [selectedDocs, setSelectedDocs] = useState([]);
+  console.log(selectedDocs);
+
+  return (
+    <>
+      <input
+        type="file"
+        // accept=".doc"
+        multiple
+        onChange={(el) =>
+          el.target.files?.length &&
+          setSelectedDocs(Array.from(el.target.files))
+        }
+      />
+
+      <DocViewer
+        documents={selectedDocs.map((file) => ({
+          uri: window.URL.createObjectURL(file),
+          fileName: file.name,
+        }))}
+        pluginRenderers={DocViewerRenderers}
+      />
+    </>
+  );
+};
+
+export default DocxViewer;
